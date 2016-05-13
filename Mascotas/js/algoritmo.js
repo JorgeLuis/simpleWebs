784afac0bc56763f9172489a00152b4b4297b7d3
Training.AAFBA.23 <Training.AAFBA.23@accenture.com>
2016-05-13 11:59:50 -0300

    zoom fotos

--- Mascotas/js/algoritmo.js
+++ Mascotas/js/algoritmo.js
@@ -1,4 +1,13 @@
-$( "button" ).click(function() {
-  $( "p" ).toggle();
+$(document).ready(function(){
+  $('#ex1').zoom();
+
 });
+
+$( "button" ).click(function() {
+
+	$( "p" ).toggle();
+
+	});
+
+
 
