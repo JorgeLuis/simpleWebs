9a0901dc3abd670d2a2c53c4bc42a7af2065b455
JorgeLuis <lescanojorgeluis@gmail.com>
2016-05-18 00:17:25 -0300

    algoritmo

--- Mascotas/js/algoritmo.js
+++ Mascotas/js/algoritmo.js
@@ -1,13 +1,105 @@
 $(document).ready(function(){
   $('#ex1').zoom();
 
+  
+
 });
 
 
-$( "#analizador" ).click(function() {
-	alert("pp");
+function buscarElMejorTPSi(){
 
-	//$( "p" ).toggle();
+	return 1;
+}
+
+function obtenerTiempoDeAtencion(){
+
+	return 2;
+}
+
+function buscarElOITCorrespondiente(i){
+
+	return 1;
+}
+
+$( "#analizador" ).click(function() {
+	
+	//1)Condiciones Iniciales
+	var Ia=[1,2,35];
+	var i =1;//esto hay que sacar;
+
+	//Variables en funcion del tiempo
+	var TPSi= buscarElMejorTPSi();
+	var IOTi= buscarElOITCorrespondiente(i);
+	var TPLL = 8;
+	var T=0;
+	var Tfinal;
+	var STLL=0;
+	var STOi=0;
+	var STS=0;
+	var STA=0;
+	
+	//Variables en enteras
+	var N=0;
+	var Ns=0;
+	var ARR=0;
+	var NT=0;
+
+	 do{
+
+		if (TPLL<TPSi){//2) y 4) se determina el tipo de evento
+			T=TPLL; //3)se avanza el tiempo hasta la proxima llegada
+			Ia=obtenerIntervaloDeArribos();
+			TPLL=T+Ia; //5)Evento futuro no condicionado
+
+			if (Ns>= 5+N) {
+				NT=NT+1;
+				Ns=Ns+1; //6)se actualiza el vertor de estado
+				STLL=STLL+TPLL;
+
+				if (Ns<=N) {
+					TPSi=buscarElMejorTPSi();
+					Ta=obtenerTiempoDeAtencion(); //7)Evento futuro condicionado
+					STA=STA+Ta;
+					TPSi=T+Ta;
+					//STOi= obtener el STO del tiempo ocioso
+					STOi=STOi+T-ITOi;
+				};
+			}else{
+				ARR= ARR+1;
+			};
+
+
+
+
+			alert(TPLL);
+
+		}else{//es una salida
+			T=TPSi; //3)se avanza al tiempo hasta la próxima salida
+			STS=STS+T;
+			Ns=Ns-1;
+
+			if (Ns>=N) {
+				Ta=obtenerTiempoDeAtencion();
+				TPSi=T+Ta;        //7)Evento futuro condicionado
+				STA=STA+Ta;
+
+			}else{
+				TPSi= 1000;//H.v
+				//IOTi=buscarElOITCorrespondiente(i);
+				IOTi=T; 
+			};
+		}//fin del else
+	} while(T>=Tfinal);//8)fin de la ejecucion
+
+	if (Ns==0) {
+		var POMi=STOi*100/T;  //9)Calcular resultados
+		var PTE = (STS-STLL-STA) * 100/NT
+	}else{//
+		TPLL=1000;//H.V. //Ver que hacer!!!
+	};
+
+	//10)Mostrar resultados
+	alert("LLego hasta el final: falta analizar más");
 
 });
 
