b438b7bc9a79a485eef784c8b7fdda10468b651c
Training.AAFBA.23 <Training.AAFBA.23@accenture.com>
2016-05-18 09:38:17 -0300

    merge

--- Mascotas/js/algoritmo.js
+++ Mascotas/js/algoritmo.js
@@ -1,3 +1,15 @@
+//HTML da la escrutura, escribe documentos
+//CSS el estilo, a documentos
+//Js el comportamient
+
+
+
+//js es un lenguaje que maneja el comportamiento de la pagina 
+//para el usuario.
+
+//js no compila nada
+//una variable es un dato, que guarda informacion
+//el scrip es una funcion que se puede volver a utilizar  
 $(document).ready(function(){
   $('#ex1').zoom();
 
